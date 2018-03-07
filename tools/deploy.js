01a3b672ea3a0168732408740b7d6375979669d3
Fredrik Antell <fred.antell@gmail.com>
2018-03-07 15:43:44 -0600

    Deploy to all 3 s3buckets at once

--- tools/deploy.js
+++ tools/deploy.js
@@ -2,18 +2,22 @@ import gulp from 'gulp';
 import task from './lib/task';
 import config from '../credentials';
 
-var s3 = require('gulp-s3-upload')(config.s3);
+const s3 = require('gulp-s3-upload')(config.s3);
+const s3Buckets = ['www.pingthings.io', 'www.pingthings.ai', 'www.predictivegrid.ai'];
+
+const pushToBucket = function pushToBucket(bucketName) {
+  return gulp.src('./build/**')
+  .pipe(s3({
+    Bucket: bucketName,
+    ACL: 'public-read',
+  }, {
+    maxRetries: 5,
+  }));
+};
 
 export default task(async function deploy() {
   process.argv.push('release');
   await require('./build')();
 
-  gulp.src("./build/**")
-    .pipe(s3({
-      Bucket: 'www.pingthings.io',
-      ACL: 'public-read'
-    }, {
-      maxRetries: 5
-    }));
-
+  s3Buckets.map(pushToBucket);
 });
