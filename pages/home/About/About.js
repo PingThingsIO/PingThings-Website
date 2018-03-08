f26c9cfd04ed2441caac386c5d5e55c235c97797
Fredrik Antell <fred.antell@gmail.com>
2018-03-08 10:47:51 -0600

    Small changes to about section

--- pages/home/About/About.js
+++ pages/home/About/About.js
@@ -1,31 +1,39 @@
-import React from 'react';
-import Scroll from 'react-scroll';
+import React from "react";
+import Scroll from "react-scroll";
 
 const Element = Scroll.Element;
-const NavTo  = Scroll.Link;
+const NavTo = Scroll.Link;
 
 export default React.createClass({
-  displayName: 'Home-About',
+  displayName: "Home-About",
 
   render() {
     return (
-      <Element name='about' className='section-about pure-g _section'>
-        <div className='pure-u-1-1 animated fadeInLeft'>
-          <div className='center-text left-align-copy'>
+      <Element name="about" className="section-about pure-g _section">
+        <div className="pure-u-1-1 animated fadeInLeft">
+          <div className="center-text left-align-copy">
             <h4>About PingThings</h4>
-            <hr className='animated slideInLeft'/>
+            <hr className="animated slideInLeft"/>
             <div className="left-align-copy">
-            <p>PingThings Inc., is a venture-backed software company that brings best-of-breed big-data software and data science techniques to the utility industry. We offer streaming analytics solutions for anomaly detection and event recognition on the power grid.</p>
-            <p>PingThings is the only company to ever receive seed funding from GE and was recognized as a top 20 early stage startup at the Siemens New Venture Forum in 2014. PingThings is engaged in several proof of concepts with multiple utilities and reliability coordinators on both coasts of North America. The data sharing agreements currently in place offer us access to data covering over one third of the North American continent. We are a very small team of seven professionals passionate about bringing to market new tools for stability and reliability.</p>
-            <p>PingThings’ PredictiveGrid™ solution is a model-free, event-driven sensor platform focused on empirical data versus traditional system models and simulations. Our loosely coupled platform enables flexibility and plug and play integration with control room systems. A comprehensive suite of data modeling technologies, including clustering and classification techniques, is applied to real-time, high-fidelity data streaming from multiple sources internal and external to the utilities. Our analytics platform enables operators to better manage, maintain and protect assets with alerts to specific events occurring now and the probability that a future event will occur, such as asset failure. PredictiveGrid™ will enable control room operators to proactively manage imminent events and potentially avoid unplanned outages and unfavorable disruptions.</p>
+              <p>
+              PingThings partners with forward thinking utilities to take advantage of a structural shift to artificial intelligence in the energy industry driven by the grid's growing complexity.
+              </p>
+              <p>
+              DOE and McKinsey estimates show predictive analytics can save utilities ~17-31% on maintenance costs. Further, unplanned outages are growing 10% every year costing $189B annually. Big data is key, but utilities use only 2% of their massive datasets. Existing software is designed for archiving, not analysis and is even worse for high data rate sensors.
+              </p>
+              <p>
+              PingThings solves these issues with the PredictiveGrid™, a purpose built platform for ingesting, storing, visualizing, analyzing, and training machine- and deep-learning models with data from large numbers of sensors. The platform is offered as an on-premise appliance and as a private cloud. Benchmarks indicate that we are two orders of magnitude faster than competitors.
+              </p>
+              <p>
+              For businesses, PredictiveGrid™ is designed to be easy to adopt and deploy. For end users, we integrate with the best industry standard open source tools available. PredictiveGrid™ solves problems at every step of the data analysis pipeline to make employees more productive, reduce the cost of analysis, and expand what's possible with your data.
+              </p>
             </div>
-            <NavTo activeClass="active" to="contact" spy={true} smooth={true} duration={750}>
+            <NavTo activeClass="active" to="contact" spy smooth duration={750}>
               <button className="pure-button">LEARN MORE</button>
             </NavTo>
           </div>
         </div>
       </Element>
-    )
-  }
-
+    );
+  },
 });
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
-import Scroll from 'react-scroll';
-import StorySection from './home/Story';
-import AboutSection from './home/About';
-import HowSection from './home/How';
-import RecommendationSection from './home/Recommendation';
-import StatsSection from './home/Stats';
-import ProductsSection from './home/Products';
-import TeamSection from './home/Team';
-import ResourcesSection from './home/Resources';
-import ContactSection from './home/Contact';
+import React, { Component } from "react";
+import Scroll from "react-scroll";
+import StorySection from "./home/Story";
+import AboutSection from "./home/About";
+import HowSection from "./home/How";
+import RecommendationSection from "./home/Recommendation";
+import StatsSection from "./home/Stats";
+import ProductsSection from "./home/Products";
+import TeamSection from "./home/Team";
+import ResourcesSection from "./home/Resources";
+import ContactSection from "./home/Contact";
 
 const Element = Scroll.Element;
-const NavTo  = Scroll.Link;
+const NavTo = Scroll.Link;
 
 export default class extends Component {
 
   render() {
-
     return (
 
-      <div id='/'>
-
+      <div id="/">
         <StorySection />
 
         <AboutSection />
