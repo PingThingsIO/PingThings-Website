ad0d740e584ceb8bce54a6df735438d9df17e090
Fredrik Antell <fred.antell@gmail.com>
2018-03-28 09:36:44 -0500

    Update deploy to push to all 3 sites

--- tools/deploy.js
+++ tools/deploy.js
@@ -3,17 +3,23 @@ import task from './lib/task';
 import config from '../credentials';
 
 var s3 = require('gulp-s3-upload')(config.s3);
+var s3Buckets = ['www.pingthings.io', 'www.pingthings.ai', 'www.predictivegrid.ai'];
 
-export default task(async function deploy() {
-  process.argv.push('release');
-  await require('./build')();
-
+var pushToBucket = (bucketName) => {
   gulp.src("./build/**")
     .pipe(s3({
-      Bucket: 'www.pingthings.io',
+      Bucket: bucketName,
       ACL: 'public-read'
     }, {
       maxRetries: 5
     }));
 
+};
+
+export default task(async function deploy() {
+  process.argv.push('release');
+  await require('./build')();
+
+  s3Buckets.map(bucketName => pushToBucket(bucketName));
+  
 });
